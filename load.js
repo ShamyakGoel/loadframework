19011cac830ddd59320fd213996305dc128c8de6
ShamyakGoel <sj907822@gmail.com>
2022-06-30 15:10:17 +0530

    Updating readme

--- load.js
+++ load.js
@@ -1,13 +1,4 @@
 (function () {
-    if (window.jQuery) {
-        checkReady(jQuery);
-    }
-    // Load the script
-    var script = document.createElement("SCRIPT");
-    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
-    script.type = 'text/javascript';
-    document.getElementsByTagName("head")[0].appendChild(script);
-
     // Poll for jQuery to come into existance
     var checkReady = function (callback) {
         if (window.jQuery) {
@@ -17,7 +8,22 @@
             window.setTimeout(function () { checkReady(callback); }, 20);
         }
     };
-
+    if (window.jQuery) {
+        checkReady(function ($) {
+            $(function () {
+                let allLoads = document.querySelectorAll('load')
+                allLoads.forEach((elem) => {
+                    let url = elem.getAttribute('from')
+                    $(elem).parent(elem).load(url)
+                })
+            });
+        });
+    }
+    // Load the script
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName("head")[0].appendChild(script);
     // Start polling...
     checkReady(function ($) {
         $(function () {
