eb06766d7da88b97ef51d5c65ef5e5ac4eae3338
ShamyakGoel <sj907822@gmail.com>
2022-06-30 15:14:35 +0530

    Bug Fixes

--- load.js
+++ load.js
@@ -1,4 +1,6 @@
 (function () {
+    window.LoadFramework = {}
+    window.LoadFramework['function'] = anony
     // Poll for jQuery to come into existance
     var checkReady = function (callback) {
         if (window.jQuery) {
@@ -38,3 +40,44 @@
         });
     });
 })();
+
+function anony(){
+        // Poll for jQuery to come into existance
+        var checkReady = function (callback) {
+            if (window.jQuery) {
+                callback(jQuery);
+            }
+            else {
+                window.setTimeout(function () { checkReady(callback); }, 20);
+            }
+        };
+        if (window.jQuery) {
+            checkReady(function ($) {
+                $(function () {
+                    let allLoads = document.querySelectorAll('load')
+                    allLoads.forEach((elem) => {
+                        let url = elem.getAttribute('from')
+                        $(elem).parent(elem).load(url)
+                    })
+                });
+            });
+        }
+        // Load the script
+        var script = document.createElement("SCRIPT");
+        script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
+        script.type = 'text/javascript';
+        document.getElementsByTagName("head")[0].appendChild(script);
+        // Start polling...
+        checkReady(function ($) {
+            $(function () {
+                let allLoads = document.querySelectorAll('load')
+                allLoads.forEach(async (elem) => {
+                    let url = elem.getAttribute('from')
+                    let html = await $.get(url)
+                    let div = $('<div></div>')
+                    div.html(html)
+                    $(elem).parent(elem).append(div)
+                })
+            });
+        });
+}
